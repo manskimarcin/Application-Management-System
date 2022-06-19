42899e2a8fe8f77a2a5c6411181046340ec5b8cc
Marcin Manski <marcin.manski1994@gmail.com>
2022-06-19 13:43:18 +0200

    Initial commit with generated source code

---
+++ src/main/java/pl/manski/marcin/applicationmanagementsystem/ApplicationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.manski.marcin.applicationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationManagementSystemApplication.class, args);
+	}
+
+}
